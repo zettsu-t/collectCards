ba2ffbd117b715c0bb589a0cc1562c34cc8655c9
Zettsu Tatsuya <zettsu.tatsuya@gmail.com>
2017-01-21 09:08:01 +0900

    Refine code

--- collectCards.hs
+++ collectCards.hs
@@ -18,7 +18,7 @@ chooseVec n r d = map f [0..d]
   where f i = (choose r i) * (choose (n - r) (d - i))
 
 -- 長さを1にする
-toUnitVec ls = map (/ (sum ls)) ls
+toUnitVec ls = map (/ sum ls) ls
 
 -- 全n種類、残りr種類で、一度にd枚引くときに 0..d枚増える確率
 -- d枚に重複がないので、残り1種類ならd/nになる。なぜなら、
@@ -29,21 +29,21 @@ toUnitVec ls = map (/ (sum ls)) ls
 probVec n r d = toUnitVec $ chooseVec n r d
 
 -- 全n種類、一度にd枚引くときに、残りr種類のときの回数の期待値を求める
-nextProbVec n r d ls = [p] ++ (take d ls)
+nextProbVec n r d ls = p : take d ls
   where ps = probVec n r d
-        qs = toUnitVec $ (drop 1 ps)
-        p = (1.0 / (1.0 - ps!!0)) + sum (zipWith (*) qs ls)
+        qs = toUnitVec $ drop 1 ps
+        p = (1.0 / (1.0 - head ps)) + sum (zipWith (*) qs ls)
 
 -- 全n種類、一度にd枚引くときに、最初の一手(残りn-d種類)までの期待値を返す
 lastProbVec n r d ls | d > (n - r) = ls
                      | otherwise = lastProbVec n (r + 1) d $ nextProbVec n r d ls
 
 -- 最初の一手は別に数える
-expectedCount n d = (f n d)!!0 + 1.0
+expectedCount n d = 1.0 + head (f n d)
   where f n d = lastProbVec n 1 d $ replicate d 0.0
 
 -- 手数と価格を返す
-solve n d price = (show count) ++ " (" ++ (show (count * price)) ++ ")"
+solve n d price = (show count) ++ " (" ++ (show $ count * price) ++ ")"
   where count = expectedCount n d
 
 -- コマンドラインから値を指定できるが、省略時の値も用意する
